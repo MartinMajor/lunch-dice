5cda9c89b1ec4529bc23fd1fe0f37dcad83fe168
Mikhail Razorvin <mikhailr@ematiq.com>
2022-11-20 22:19:55 +0100

    added simulation into main app

--- src/Diner.js
+++ src/Diner.js
@@ -1,33 +1,35 @@
 import React from 'react';
+import { DinnerPricesCtx, DinnerValuesCtx } from "./Context";
 import './Diner.css';
 
-class Diner extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: null,
-            price: null
-        };
-    }
+console.log(DinnerPricesCtx, DinnerValuesCtx);
 
-    render() {
-        return (
-            <div className="diner">
-                <label htmlFor={"number" + this.props.idx}>Price</label>
-                <input className="price" type="number" id={"number" + this.props.idx} onChange={event =>this.setState({price: event.target.value})} />
-                <button className="roll" disabled={!this.state.price} onClick={() =>
-                    fetch("https://www.random.org/integers//?num=1&min=1&max=10000&col=1&base=10&format=plain&rnd=new")
-                        .then((value) => value.text())
-                        .then((number) => this.setState({
-                            number: Number(number)
-                        }))
-                }>Roll
-                </button>
-                <br />
-                Rolled: <span>{this.state.number ? this.state.number : '?'}</span>, normalized: <b>{this.state.number ? Math.round(this.state.number / this.state.price * 100) / 100 : '?'}</b>
-            </div>
-        );
-    }
+
+export function Diner(props) {
+    const pricesCtx = React.useContext(DinnerPricesCtx);
+    const valuesCtx = React.useContext(DinnerValuesCtx);
+    const [guestRandomNumber, setGuestRandomNumber] = React.useState(null);
+    const dinnerPrice = pricesCtx.values[props.idx] ?? "";
+    const dinnerValue = valuesCtx.values[props.idx] ?? null;
+
+    return (
+        <div className="diner">
+            <label htmlFor={"number" + props.idx}>Price</label>
+            <input value={dinnerPrice} className="price" type="number" id={"number" + props.idx} onChange={event => pricesCtx.setValues({ ...pricesCtx.values, [props.idx]: event.currentTarget.value })} />
+            value: {dinnerValue ? Math.round(dinnerValue) : "- "}
+            <button className="roll" disabled={!dinnerValue} onClick={() =>
+                fetch("https://www.random.org/integers//?num=1&min=1&max=10000&col=1&base=10&format=plain&rnd=new")
+                    .then((value) => value.text())
+                    .then(setGuestRandomNumber)
+            }>Roll
+
+            </button>
+            <br />
+            Rolled: <span>{guestRandomNumber ? guestRandomNumber : '?'}</span>, normalized: <b>{guestRandomNumber ? Math.round(guestRandomNumber / dinnerValue * 100) / 100 : '?'}</b>
+        </div>
+
+    );
 }
+
 
 export default Diner;
