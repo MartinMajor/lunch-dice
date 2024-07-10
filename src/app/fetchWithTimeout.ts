76636cd8d8fe18444e6433905159b8eb948cf241
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2024-07-10 17:37:49 +0200

    refactor: Update codebase

--- src/app/fetchWithTimeout.ts
+++ src/app/fetchWithTimeout.ts
@@ -1,17 +1,20 @@
 // adds option timeout (in ms)
-async function fetchWithTimeout(resource: RequestInfo | URL, options: Partial<RequestInit> & { timeout?: number } = {}) {
-    const { timeout = 10000 } = options;
+async function fetchWithTimeout(
+  resource: RequestInfo | URL,
+  options: Partial<RequestInit> & { timeout?: number } = {}
+) {
+  const { timeout = 10000 } = options;
 
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
 
-    const response = await fetch(resource, {
-        ...options,
-        signal: controller.signal
-    });
-    clearTimeout(id);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
 
-    return response;
+  return response;
 }
 
 export default fetchWithTimeout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,33 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react';
-import Dice from './components/Dice/Dice'
-import Diners from './components/Diners/Diners'
-import Header from './components/Header/Header'
-import { RollFunctionContext, offlineRollFunction, onlineRollFunction } from './RollFunctionContext';
+import {
+  RollFunctionContext,
+  offlineRollFunction,
+  onlineRollFunction,
+} from './RollFunctionContext';
+import Dice from './components/Dice/Dice';
+import Diners from './components/Diners/Diners';
+import Header from './components/Header/Header';
 
 export default function Home() {
-
   const [rollFunction, setRollFunction] = useState(() => onlineRollFunction);
 
-  const stateHandler = (state: Boolean) => {
-    setRollFunction(() => state ? onlineRollFunction : offlineRollFunction);
-  }
+  const stateHandler = (state: boolean) => {
+    setRollFunction(() => (state ? onlineRollFunction : offlineRollFunction));
+  };
 
   return (
     <>
       <RollFunctionContext.Provider value={rollFunction}>
         <Header stateHandler={stateHandler} />
-        <main className="flex flex-wrap max-w-4xl p-4 m-auto">
+        <main className='flex flex-wrap max-w-4xl p-4 m-auto'>
           <Dice />
-          <div className="w-[32rem] mx-auto">
+          <div className='w-[32rem] mx-auto'>
             <Diners />
           </div>
         </main>
       </RollFunctionContext.Provider>
     </>
-  )
+  );
 }
