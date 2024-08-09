4137fc94f9d695243e6a855f41760563c3875996
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2024-08-09 09:24:56 +0200

    refactor: Update codebase

--- src/app/components/OnlineSwitcher/OnlineSwitcher.tsx
+++ src/app/components/OnlineSwitcher/OnlineSwitcher.tsx
@@ -4,18 +4,18 @@ function OnlineSwitcher({ stateHandler }: { stateHandler: (state: boolean) => vo
   const [state, setState] = useState(true);
 
   return (
-    <label className='relative inline-flex items-center cursor-pointer'>
+    <label className='relative inline-flex cursor-pointer items-center'>
       <input
         type='checkbox'
         value='1'
         checked={state}
-        className='sr-only peer'
+        className='peer sr-only'
         onChange={(e) => {
           setState(e.target.checked);
           stateHandler(e.target.checked);
         }}
       />
-      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-offline peer-checked:after:bg-online after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600" />
+      <div className='peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-offline after:transition-all after:content-[""] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-checked:after:bg-online peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300' />
     </label>
   );
 }
